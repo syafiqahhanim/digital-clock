52767830e32de2ae5b484dea7484dfe14b527312
syafiqahhanim <m-10701212@moe-dl.edu.my>
2024-09-05 14:43:55 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function updateTime() {
+  const timeElement = document.getElementById('time');
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).pasStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateTime, 1000);
+updareTime();
